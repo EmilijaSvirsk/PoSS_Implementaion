a06ea6938daef1d8e98316c0cc45b1a724073e28
povilassl <slivinskas.povilas@gmail.com>
2022-11-18 19:46:43 +0200

    implementing yaml generation

--- Program.cs
+++ Program.cs
@@ -12,12 +12,10 @@ builder.Services.AddScoped<IRandomizer, Randomizer>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
+builder.Services.AddSwaggerGen(c => {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
 
-    c.TagActionsBy(api =>
-    {
+    c.TagActionsBy(api => {
         if (api.GroupName != null)
         {
             return new[] { api.GroupName };
@@ -44,14 +42,14 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
 
-    /*
-     * Kazkokiu panasiu metodu turetu sugeneruot yaml
-     * https://stackoverflow.com/questions/45100923/generate-yaml-swagger-using-swashbuckle
-    app.UseSwaggerUI(x => { x.SwaggerEndpoint("/v1/swagger.yaml", "Komandux project API"); });
-    */
+    //Tai dabar, kad sugeneruoti Yaml, reikia paleist programa ir webpage virsuj po uzrasu API
+    //yra linkas i /swagger/v1/swagger.yaml, is cia galima gauti yaml faila.
+    //O kad generuoti html, tai reikia ta yaml faila imest i cia - https://editor.swagger.io/
+    //ten yra galimybiu generuot pagal yaml visokius kitus failus
+    app.UseSwaggerUI(x => { x.SwaggerEndpoint("/swagger/v1/swagger.yaml", "Komandux project API"); });
 }
 
 app.UseHttpsRedirection();
